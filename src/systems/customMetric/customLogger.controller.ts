a06ee85f5047528ac8522e1830ead2806786d45b
Lee Djen Sep <leedjensep@gmail.com>
2025-11-19 22:01:09 +0500

    delete controller

--- src/systems/customMetric/customLogger.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { CustomLoggerService } from './customLogger.service';
-
-@Controller('custom-logger')
-export class CustomLoggerController {
-  constructor(private readonly customLoggerService: CustomLoggerService) {}
-}
